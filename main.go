cd22dc8dfb076ba943b630a6d86ba47ac0008028
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2020-01-07 10:37:23 +0700

    init

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"html/template"
+	"io"
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+// TemplateRenderer is a custom html/template renderer for Echo framework
+type TemplateRenderer struct {
+	templates *template.Template
+}
+
+// Render renders a template document
+func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+
+	// Add global methods if data is a map
+	if viewContext, isMap := data.(map[string]interface{}); isMap {
+		viewContext["reverse"] = c.Echo().Reverse
+	}
+
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func userHandler(c echo.Context) error {
+	return c.Render(http.StatusOK, "template.html", map[string]interface{}{
+		"name": "Ocki Bagus Pratama!",
+	})
+}
+
+func main() {
+	e := echo.New()
+
+	renderer := &TemplateRenderer{
+		templates: template.Must(template.ParseGlob("*.html")),
+	}
+	e.Renderer = renderer
+
+	// Named route "main"
+	e.GET("/", userHandler).Name = "main"
+
+	e.Start(":8000")
+}
