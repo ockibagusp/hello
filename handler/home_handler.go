6a00b6e4757f64e341be43b75e7b42373afa94be
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2020-01-11 14:29:25 +0700

    home and about

---
+++ handler/home_handler.go
@@ -0,0 +1,17 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+// HomeHandler "home.html"
+func HomeHandler(c echo.Context) error {
+	// Please note the the second parameter "home.html" is the template name and should
+	// be equal to one of the keys in the TemplateRegistry array defined in main.go
+	return c.Render(http.StatusOK, "home.html", map[string]interface{}{
+		"name": "Home",
+		"msg":  "Ocki Bagus Pratama!",
+	})
+}
--- main.go
+++ main.go
@@ -1,101 +1,51 @@
 package main
 
 import (
+	"errors"
 	"html/template"
 	"io"
-	"net/http"
-	"strconv"
 
+	"github.com/OckiFals/hello/handler"
 	"github.com/labstack/echo"
-)
-
-type (
-	user struct {
-		ID   int    `json:"id"`
-		Name string `json:"name"`
-	}
-)
-
-var (
-	users = map[int]*user{}
-	seq   = 1
 )
 
 // TemplateRenderer is a custom html/template renderer for Echo framework
 type TemplateRenderer struct {
-	templates *template.Template
+	templates map[string]*template.Template
 }
 
-// Render renders a template document
+// Render implement e.Renderer interface
 func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-
 	// Add global methods if data is a map
 	if viewContext, isMap := data.(map[string]interface{}); isMap {
 		viewContext["reverse"] = c.Echo().Reverse
 	}
 
-	return t.templates.ExecuteTemplate(w, name, data)
-}
-
-func mainHandler(c echo.Context) error {
-	return c.Render(http.StatusOK, "index.html", map[string]interface{}{
-		"name": "Ocki Bagus Pratama!",
-	})
-}
-
-//----------
-// Handlers
-//----------
-
-func createUser(c echo.Context) error {
-	u := &user{
-		ID: seq,
-	}
-	if err := c.Bind(u); err != nil {
+	tmpl, ok := t.templates[name]
+	if !ok {
+		err := errors.New("Template not found -> " + name)
 		return err
 	}
-	users[u.ID] = u
-	seq++
-	return c.JSON(http.StatusCreated, u)
-}
+	return tmpl.ExecuteTemplate(w, "base.html", data)
 
-func getUser(c echo.Context) error {
-	id, _ := strconv.Atoi(c.Param("id"))
-	return c.JSON(http.StatusOK, users[id])
-}
-
-func updateUser(c echo.Context) error {
-	u := new(user)
-	if err := c.Bind(u); err != nil {
-		return err
-	}
-	id, _ := strconv.Atoi(c.Param("id"))
-	users[id].Name = u.Name
-	return c.JSON(http.StatusOK, users[id])
-}
-
-func deleteUser(c echo.Context) error {
-	id, _ := strconv.Atoi(c.Param("id"))
-	delete(users, id)
-	return c.NoContent(http.StatusNoContent)
 }
 
 func main() {
+	// Echo instance
 	e := echo.New()
 
-	renderer := &TemplateRenderer{
-		templates: template.Must(template.ParseGlob("*.html")),
+	// Instantiate a template registry with an array of template set
+	templates := make(map[string]*template.Template)
+	templates["home.html"] = template.Must(template.ParseFiles("views/home.html", "views/base.html"))
+	templates["about.html"] = template.Must(template.ParseFiles("views/about.html", "views/base.html"))
+	e.Renderer = &TemplateRenderer{
+		templates: templates,
 	}
-	e.Renderer = renderer
 
-	// Named route "main"
-	e.GET("/", mainHandler).Name = "main"
+	// Route => handler
+	e.GET("/", handler.HomeHandler).Name = "home"
+	e.GET("/about", handler.AboutHandler).Name = "about"
 
-	// Routes
-	e.POST("/users", createUser)
-	e.GET("/users/:id", getUser)
-	e.PUT("/users/:id", updateUser)
-	e.DELETE("/users/:id", deleteUser)
-
+	// Start the Echo server
 	e.Start(":8000")
 }
