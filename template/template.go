f51787d7dc6591e9092104788e8ff14c5158e873
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2021-12-07 21:07:27 +0700

    [7/12/21] fix template to "user-view-password.html"

--- template/template.go
+++ template/template.go
@@ -20,12 +20,25 @@ func NewTemplates() *Templates {
 	t := make(map[string]*template.Template)
 	t["home.html"] = parseFiles("views/home.html")
 	t["about.html"] = parseFiles("views/about.html")
-	t["login.html"] = template.Must(template.ParseFiles("views/login.html"))
+	// t["login.html"] = parseFiles("views/login.html") -> base.html
+	t["login.html"] = template.Must(
+		template.ParseFiles("views/login.html"),
+	)
 	t["users/user-all.html"] = parseFiles("views/users/user-all.html")
 	t["users/user-add.html"] = parseFiles("views/users/user-add.html", "views/users/user-form.html")
 	t["users/user-read.html"] = parseFiles("views/users/user-read.html", "views/users/user-form.html")
 	t["users/user-view.html"] = parseFiles("views/users/user-view.html", "views/users/user-form.html")
-	t["users/user-view-password.html"] = parseFiles("views/users/user-view-password.html")
+	/*
+		t["users/user-view-password.html"] -> no
+			{..,"status":500,"error":"html/template: \"users/user-view-password.html\" is undefined",..}
+			why?
+		t["user-view-password.html"] -> yes
+		----
+		t["user-view-password.html"] = parseFiles("views/users/user-view-password.html") -> base.html
+	*/
+	t["user-view-password.html"] = template.Must(
+		template.ParseFiles("views/users/user-view-password.html"),
+	)
 
 	return &Templates{
 		Templates: t,
@@ -44,9 +57,15 @@ func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Co
 		viewContext["reverse"] = c.Echo().Reverse
 
 		/*
-			@param /login
+			@param /login and /users/view/:id/password
 			- Is HTML Only
 			is_html_only (bool): {true, false}
+
+			TODO:
+			Login name: 				"login.html -> ok
+			UpdateUserByPassword name: 	"user-view-password.html" -> ok
+										"users/user-view-password.html" -> no
+			why?
 		*/
 		if viewContext["is_html_only"] == true {
 			return tmpl.ExecuteTemplate(w, name, data)
