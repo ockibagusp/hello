b4c874cc09d6e2fbe60ee36726562d4fea121c06
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2021-12-23 21:18:21 +0700

    [23/12/21] new session method

--- test/session_controller_test.go
+++ test/session_controller_test.go
@@ -8,6 +8,11 @@ import (
 	"github.com/ockibagusp/hello/types"
 )
 
+const GET int = 1
+
+// POST int = 2
+const POST = 2
+
 func TestLogin(t *testing.T) {
 	noAuth := setupTestServer(t)
 
@@ -18,16 +23,23 @@ func TestLogin(t *testing.T) {
 		Email:    "ocki.bagus.p@gmail.com",
 		Password: "$2a$10$Y3UewQkjw808Ig90OPjuq.zFYIUGgFkWBuYiKzwLK8n3t9S8RYuYa",
 		Name:     "Ocki Bagus Pratama",
-		City:     2,
 	}.Save(db)
 
 	testCases := []struct {
+		method int
 		name   string
 		user   types.LoginForm
 		status int
 	}{
 		{
-			name: "login success",
+			method: GET,
+			name:   "login get",
+			// HTTP response status: 200 OK
+			status: http.StatusOK,
+		},
+		{
+			method: POST,
+			name:   "login success",
 			user: types.LoginForm{
 				Username: "ockibagusp",
 				Password: "user123",
@@ -36,7 +48,8 @@ func TestLogin(t *testing.T) {
 			status: http.StatusOK,
 		},
 		{
-			name: "login failure",
+			method: POST,
+			name:   "login failure",
 			user: types.LoginForm{
 				Username: "ockibagusp",
 				Password: "<bad password>",
@@ -48,6 +61,13 @@ func TestLogin(t *testing.T) {
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
+			if tc.method == GET {
+				noAuth.GET("/login").
+					Expect().
+					Status(tc.status)
+				return
+			}
+			// tc.method == POST
 			noAuth.POST("/login").WithForm(tc.user).
 				Expect().
 				Status(tc.status)
